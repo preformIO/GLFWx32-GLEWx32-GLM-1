723435047357fb808c78b8934450c4340895eeb0
david allen <allend5@rpi.edu>
2022-02-19 00:59:46 -0500

    Obj Loader and Shader classes

---
+++ src/objloader.h
@@ -0,0 +1,20 @@
+#include <glm/glm.hpp>
+#include <vector>
+
+// author: david allen <d@preform.io>
+// based on code from: http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+
+#ifndef H_OBJLOADER
+#define H_OBJLOADER
+
+namespace lOBJ {
+    bool loadOBJ(
+        // const char * path,
+        // std::vector < glm::vec3 > & out_vertices,
+        // std::vector < glm::vec2 > & out_uvs,
+        // std::vector < glm::vec3 > & out_normals
+    );
+}
+
+
+#endif //!H_OBJLOADER
