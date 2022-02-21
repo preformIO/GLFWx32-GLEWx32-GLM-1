e7dd146f203410ae95efefba1ac87c283a664e36
david allen <allend5@rpi.edu>
2022-02-21 10:06:01 -0500

    Doxygen style application description, updated readme to match.

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,3 @@
-// Author: david aloka <d@preform.io>
-// History:
-//      - project setup https://www.wikihow.com/Set-Up-OpenGL-GLFW-GLEW-GLM-on-a-Project-with-Visual-Studio
-// 		- initial source code https://github.com/opengl-tutorials/ogl/blob/master/tutorial01_first_window/tutorial01.cpp
-//		- additional source code https://github.com/vkphillia/opengl-cpp-template
-// 		- integrated "helloTriangle.cpp" https://www.dropbox.com/s/ldzgp57ph9btzxf/helloTriangle.cpp?dl=0
-//		- perspective projection camera https://learnopengl.com/Getting-started/Coordinate-Systems
-
 // Include standard headers
 #include <stdlib.h>
 #include <stdio.h>
@@ -51,6 +43,39 @@ const char* pathOBJ = "data/objs/cube_tris.obj"; // ? relative to final applicat
 
 
 
+/// <summary>
+///		Basic OpenGL application that loads an OBJ file from pathOBJ
+///		and displays it using shaders specified by pathVShader 
+///		and pathFshader using perspective projection and slow model rotation.
+/// 
+///		User can press escape to quit the appliction, and re-sizing the window
+///		resizes the perspective projection so it diplays without distortion
+///		regardless of window size.
+/// </summary>
+/// <author>
+///		david aloka <d@preform.io>, copyright 2022
+/// </author>
+/// <history>
+///		2022.02.19 12:50
+///			- implemented perspective projection camera including 
+///			model, view, and projection transformations
+///			https://learnopengl.com/Getting-started/Coordinate-Systems
+///			- implemented simple OBJ loader
+///			http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+///		2022.02.19 00:25
+///			- integrated Shader class
+///			https://learnopengl.com/code_viewer_gh.php?code=includes/learnopengl/shader.h
+///			- integrated "helloTriangle.cpp" 
+///			https://www.dropbox.com/s/ldzgp57ph9btzxf/helloTriangle.cpp?dl=0
+///			- integrated additional source code 
+///			https://github.com/vkphillia/opengl-cpp-template
+///		2022.02.19 00:25
+/// 		- integrated initial source code 
+///			https://github.com/opengl-tutorials/ogl/blob/master/tutorial01_first_window/tutorial01.cpp
+///			- project setup 
+///			https://www.wikihow.com/Set-Up-OpenGL-GLFW-GLEW-GLM-on-a-Project-with-Visual-Studio
+/// </history>
+/// <returns>0</returns>
 int main(void)
 {
 	// glfw: initialize and configure
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-GLFWx32-GLEWx32-GLM-1
+# GLFWx32-GLEWx32-GLM-1
+
+Basic OpenGL application that loads an OBJ file from pathOBJ and displays it using shaders specified by pathVShader and pathFshader using perspective projection and slow model rotation.
+
+User can press escape to quit the appliction, and re-sizing the window resizes the perspective projection so it diplays without distortion regardless of window size.
+
+## author
+
+david aloka <d@preform.io>, copyright 2022
+
+## history
+
+### ### 2022.02.19 12:50
+ * implemented perspective projection camera including model, view, and projection transformations
+	https://learnopengl.com/Getting-started/Coordinate-Systems
+ * implemented simple OBJ loader
+	http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+### 2022.02.19 00:25
+ * integrated Shader class
+	https://learnopengl.com/code_viewer_gh.php?code=includes/learnopengl/shader.h
+ * integrated "helloTriangle.cpp" 
+	https://www.dropbox.com/s/ldzgp57ph9btzxf/helloTriangle.cpp?dl=0
+ * integrated additional source code 
+	https://github.com/vkphillia/opengl-cpp-template
+### 2022.02.19 00:25
+ * integrated initial source code 
+	https://github.com/opengl-tutorials/ogl/blob/master/tutorial01_first_window/tutorial01.cpp
+ * project setup 
+	https://www.wikihow.com/Set-Up-OpenGL-GLFW-GLEW-GLM-on-a-Project-with-Visual-Studio
